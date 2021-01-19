6009c3026b0555ccca820d157d693b817e2e0788
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2021-01-20 03:41:07 +0530

    Create Structure_Padding.c

---
+++ EXTRA/Structure_Padding.c
@@ -0,0 +1,61 @@
+#include <stdio.h> 
+
+// Alignment requirements 
+// (typical 32 bit machine) 
+  
+// char         1 byte 
+// short int    2 bytes 
+// int          4 bytes 
+// double       8 bytes 
+  
+// structure A 
+typedef struct structa_tag 
+{ 
+   char        c; //1+1
+   short int   s; //2
+} structa_t; 
+  
+// structure B 
+typedef struct structb_tag 
+{ 
+   short int   s; //2
+   char        c; //1+1
+   int         i; //4
+} structb_t; 
+  
+// structure C 
+typedef struct structc_tag 
+{ 
+   char        c; //1+7
+   double      d; //8
+   int         s; //4+4
+} structc_t; 
+  
+// structure D 
+typedef struct structd_tag 
+{ 
+   int         e; //4+4
+   double      d; //8
+   int         s; //4
+   char        c; //1+3
+} structd_t; 
+  
+int main() 
+{ 
+   printf("sizeof(structa_t) = %d\n", sizeof(structa_t)); 
+   printf("sizeof(structb_t) = %d\n", sizeof(structb_t)); 
+   printf("sizeof(structc_t) = %d\n", sizeof(structc_t)); 
+   printf("sizeof(structd_t) = %d\n", sizeof(structd_t)); 
+  
+   return 0; 
+} 
+
+/*
+                OUTPUT
+                
+  sizeof(structa_t) = 4
+  sizeof(structb_t) = 8
+  sizeof(structc_t) = 24
+  sizeof(structd_t) = 24
+  
+  */
